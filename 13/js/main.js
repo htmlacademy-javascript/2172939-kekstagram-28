0436d02fdf4f4404a79789bf56e57e1dcd401948
Keks <keks@htmlacademy.ru>
2023-03-28 19:29:34 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,7 @@
+import {getPictures} from './data.js';
+import {renderGallery} from './gallery.js';
+import './form-validation.js';
+import './scale-picture-in-form.js';
+import './effect-picture-in-form.js';
+
+renderGallery(getPictures());
