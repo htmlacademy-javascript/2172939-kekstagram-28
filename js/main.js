28cda74f3d7c52d09375d3dc9ebce68a5430ca53
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-01 20:12:01 +0300

    собирает ф-ии при отправке и получении данных

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-import {getPictures} from './data.js';
+import {getData, sendData} from './api.js';
 import {renderGallery} from './gallery.js';
+import {setOnFormSubmit, closeModal} from './form-validation.js';
+import {createSuccessMessage, createErrorMessage} from './message.js';
+import {showAlert} from './util.js';
 import './form-validation.js';
 import './scale-picture-in-form.js';
 import './effect-picture-in-form.js';
 
-renderGallery(getPictures());
+// getData()
+//   .then((pictures) => {
+//     renderGallery(pictures);
+//   })
+//   .catch((err) => {
+//     showAlert(err.message);
+//   });
+// setOnFormSubmit(closeModal);
+
+try {
+  const data = await getData();
+  renderGallery(data);
+} catch (err) {
+  showAlert(err.message);
+}
+
+setOnFormSubmit (async (data) => {
+  try {
+    await sendData(data);
+    closeModal();
+    createSuccessMessage();
+  } catch {
+    createErrorMessage();
+  }
+});
