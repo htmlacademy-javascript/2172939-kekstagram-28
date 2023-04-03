42f2fbd42963453041d63ac19a2d767f3ac8103a
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-03 22:01:31 +0300

    добавляет ф-ю debounce для устранения дребезга

--- js/util.js
+++ js/util.js
@@ -21,6 +21,25 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+//Функция debounce для устранения дребезга
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
 //функция-генератор для получения случайного числа
 
 const getRandomInteger = (min, max) => {
@@ -55,4 +74,4 @@ const createRandomIdFromRangeGenerator = (min, max) => {
 //проверка нажатой клавиши Esc
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomInteger, getRandomArrayElement, createRandomIdFromRangeGenerator, isEscapeKey, showAlert};
+export {getRandomInteger, getRandomArrayElement, createRandomIdFromRangeGenerator, isEscapeKey, showAlert, debounce};
