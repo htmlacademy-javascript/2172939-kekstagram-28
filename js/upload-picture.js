744d00de9faa3e67f16afc2f28207c0eb9ee78dc
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-06 16:41:52 +0300

    добавляет фото юзера в превью и доп-но в эффекты

--- js/upload-picture.js
+++ js/upload-picture.js
@@ -1,12 +1,11 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const fileChooser = document.querySelector('.img-upload__start input[type=file]');
-const preview = document.querySelector('.img-upload__preview img');
+const previewImage = document.querySelector('.img-upload__preview img');
+const previewEffectsImages = document.querySelectorAll('.effects__preview');
 
-// событие случится, когда пользователь выберет изображение для загрузки
-//cвойство files — это структура, похожая на массив. выбираем только 1 файл, плучается у него индекс 0
-//URL.createObjectURL() позволяет сделать ссылку на содержимое, а не ресурс на каком-то адресе
 
+// событие случится, когда пользователь выберет изображение для загрузки. URL.createObjectURL() позволяет сделать ссылку на содержимое
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
@@ -15,6 +14,13 @@ fileChooser.addEventListener('change', () => {
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    preview.src = URL.createObjectURL(file);
+    previewImage.src = URL.createObjectURL(file);
   }
+
+  previewEffectsImages.forEach(
+    (image) => {
+      image.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    });
 });
+
+
