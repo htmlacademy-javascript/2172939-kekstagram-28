3c1fc126cb294392d45019757d8070d10892f597
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-06 16:38:34 +0300

    комментарий для обсуждения

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,6 @@
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture-modal.js';
 
-
 const container = document.querySelector('.pictures');
 
 const renderGallery = (pictures) => {
@@ -20,5 +19,26 @@ const renderGallery = (pictures) => {
 
   renderThumbnails(pictures, container);
 };
+
+// // из ретро
+// let pictures = [];
+// const onContainerClick = (evt) => {
+//   const thumbnail = evt.target.closest('[data-thumbnail-id]');
+//   if (!thumbnail) {
+//     return;
+//   }
+
+//   evt.preventDefault();
+//   const picture = pictures.find(
+//     (item) => item.id === +thumbnail.dataset.thumbnailId
+//   );
+//   openBigPicture(picture);
+// };
+
+// const renderGallery = (currentPictures) => {
+//   pictures = currentPictures;
+//   renderThumbnails(pictures, container);
+//   container.addEventListener('click', onContainerClick);
+// };
 
 export {renderGallery};
