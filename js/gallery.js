44fd9a52eeb1b2eb3f7853f249e6e71282b2d582
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-06 19:56:30 +0300

    рефакторит код

--- js/gallery.js
+++ js/gallery.js
@@ -1,44 +1,27 @@
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture-modal.js';
 
-const container = document.querySelector('.pictures');
+const thumbnailContainer = document.querySelector('.pictures');
 
-const renderGallery = (pictures) => {
-  container.addEventListener('click', (evt) => {
-    const thumbnail = evt.target.closest('[data-thumbnail-id]');
-    if (!thumbnail) {
-      return;
-    }
+let pictures = [];
 
-    evt.preventDefault();
-    const picture = pictures.find(
-      (item) => item.id === +thumbnail.dataset.thumbnailId
-    );
-    openBigPicture(picture);
-  });
+const onContainerClick = (evt) => {
+  const thumbnail = evt.target.closest('[data-thumbnail-id]');
+  if (!thumbnail) {
+    return;
+  }
 
-  renderThumbnails(pictures, container);
+  evt.preventDefault();
+  const picture = pictures.find(
+    (item) => item.id === +thumbnail.dataset.thumbnailId
+  );
+  openBigPicture(picture);
 };
 
-// // из ретро
-// let pictures = [];
-// const onContainerClick = (evt) => {
-//   const thumbnail = evt.target.closest('[data-thumbnail-id]');
-//   if (!thumbnail) {
-//     return;
-//   }
-
-//   evt.preventDefault();
-//   const picture = pictures.find(
-//     (item) => item.id === +thumbnail.dataset.thumbnailId
-//   );
-//   openBigPicture(picture);
-// };
-
-// const renderGallery = (currentPictures) => {
-//   pictures = currentPictures;
-//   renderThumbnails(pictures, container);
-//   container.addEventListener('click', onContainerClick);
-// };
+const renderGallery = (currentPictures) => {
+  pictures = currentPictures;
+  thumbnailContainer.addEventListener('click', onContainerClick);
+  renderThumbnails(pictures);
+};
 
 export {renderGallery};
