44d7efee62530baded70f85eb9043ea523eb0a92
Anastasiia Komleva <asyakoma1998@gmail.com>
2023-04-06 20:00:23 +0300

    подключает модули, используя просто промисы

--- js/main.js
+++ js/main.js
@@ -1,49 +1,20 @@
-import {getData, sendData} from './api.js';
+import {getData} from './api.js';
 import {renderGallery} from './gallery.js';
 import {setOnFormSubmit, closeModal} from './form-validation.js';
-import {createSuccessMessage, createErrorMessage} from './message.js';
-import {showAlert} from './util.js';
-import './form-validation.js';
+import {init, getFiltredPictures} from './filter-gallery.js';
+import {showAlert, debounce} from './util.js';
 import './scale-picture-in-form.js';
 import './effect-picture-in-form.js';
 import './upload-picture.js';
 
-//для 11-2 вариант с промисами
-// getData()
-//   .then((pictures) => {
-//     renderGallery(pictures);
-//   })
-//   .catch((err) => {
-//     showAlert(err.message);
-//   });
-// setOnFormSubmit(closeModal);
+getData()
+  .then((data) => {
+    const debouncedRenderGallery = debounce(renderGallery);
+    init(data, debouncedRenderGallery);
+    renderGallery(getFiltredPictures());
+  })
+  .catch((err) => {
+    showAlert(err.message);
+  });
 
-try {
-  const data = await getData();
-  renderGallery(data);
-} catch (err) {
-  showAlert(err.message);
-}
-
-setOnFormSubmit (async (data) => {
-  try {
-    await sendData(data);
-    closeModal();
-    createSuccessMessage();
-  } catch {
-    createErrorMessage();
-  }
-});
-
-
-// // //для 12-1 ретро
-// import {showAlert, debounce} from './util.js';
-// import {init, getFiltredPictures} from './filter-gallery.js';
-// try {
-//   const data = await getData();
-//   const debouncedRenderGallery = debounce(renderGallery);
-//   init(data, debouncedRenderGallery);
-//   renderGallery(getFiltredPictures());
-// } catch (err) {
-//   showAlert(err.message);
-// }
+setOnFormSubmit(closeModal);
